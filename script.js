7e9ce9ee5ff2b896860749cc87b82c6666425aa2
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-10-24 14:26:18 +0600

    play button and poss button functioning

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-const musicContainer = document.getElementById("music_container");
-
+const musicContainer = document.getElementById("music-container");
 const playBtn = document.getElementById("play");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
-
 const audio = document.getElementById("audio");
 const progress = document.getElementById("progress");
 const progressContainer = document.getElementById("progress-container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
 
-// song titel...
 const songs = ["hey", "summer", "ukulele"];
-
-// keep track of song..
 let songIndex = 2;
 
-// initially load song details into Dom..
 loadSong(songs[songIndex]);
 
-// update song details..
 function loadSong(song) {
-  title.innerHTML = song;
+  title.innerText = song;
   audio.src = `music/${song}.mp3`;
-  cover.src = `img/${song}.jpg`;
+  cover.src = `images/${song}.jpg`;
+}
+
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fa-solid").classList.remove("fa-play");
+  playBtn.querySelector("i.fa-solid").classList.add("fa-pause");
+  audio.play();
+}
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fa-solid").classList.add("fa-play");
+  playBtn.querySelector("i.fa-solid").classList.remove("fa-pause");
+  audio.pause();
+}
+
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fa-solid").classList.remove("fa-pause");
+  playBtn.querySelector("i.fa-solid").classList.add("fa-play");
+  audio.pause();
 }
