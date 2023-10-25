951c10d60bb1f1b7937fe23ba735ba204d85cebc
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-10-25 18:13:38 +0600

    complet js section

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ let songIndex = 2;
 
 loadSong(songs[songIndex]);
 
-function loadSong(song) {
-  title.innerText = song;
-  audio.src = `music/${song}.mp3`;
-  cover.src = `images/${song}.jpg`;
+function loadSong(songs) {
+  title.innerText = songs;
+  audio.src = `songs/${songs}.mp3`;
+  cover.src = `images/${songs}.jpg`;
 }
 
 function playSong() {
@@ -32,6 +32,39 @@ function pauseSong() {
   audio.pause();
 }
 
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+function nextSong() {
+  songIndex++;
+  if (songIndex >= songs.length) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function updateProgress() {
+  const { duration, currentTime } = audio;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
 playBtn.addEventListener("click", () => {
   const isPlaying = musicContainer.classList.contains("play");
 
@@ -42,9 +75,11 @@ playBtn.addEventListener("click", () => {
   }
 });
 
-function pauseSong() {
-  musicContainer.classList.remove("play");
-  playBtn.querySelector("i.fa-solid").classList.remove("fa-pause");
-  playBtn.querySelector("i.fa-solid").classList.add("fa-play");
-  audio.pause();
-}
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+audio.addEventListener("timeupdate", updateProgress);
+
+progressContainer.addEventListener("click", setProgress);
+
+audio.addEventListener("ended", nextSong);
